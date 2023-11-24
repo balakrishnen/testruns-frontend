a67a467de5c4dc4db48c87fe781f820917f73ab9
kalaivani <kalaivani.ganesan@twilightsoftwares.com>
2023-11-24 16:22:55 +0530

    user signup

--- src/api/userAPI.ts
+++ src/api/userAPI.ts
@@ -2,13 +2,18 @@ import { fetchUserFailure, fetchUserStart, fetchUserSuccess } from '../features/
 import { POST_USER,GET_USER } from '../graphql/users/users.graphql';
 import { client } from '../utils/config';
 
-export const postUserData = (payload: any) => async () => {
+export const postUserData = (payload: any) => async (dispatch: any) => {
+  dispatch(fetchUserStart());
   try {
     const response = await client.mutate({
       mutation: POST_USER,
       variables: payload,
     });
-  } catch (error: any) {}
+    console.log(response);
+    dispatch(fetchUserSuccess(response.data));
+  } catch (error: any) {
+    dispatch(fetchUserFailure(error.message));
+  }
 };
 
 export const fetchGetUser = (payload: any) => async (dispatch: any) => {
