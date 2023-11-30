c5a446fe3016b4d342b9e037143e46639f36ecb3
kalaivani <kalaivani.ganesan@twilightsoftwares.com>
2023-11-30 20:23:51 +0530

    issue fixed in runz procedure texteditor

--- src/pages/procedures/details/[id].tsx
+++ src/pages/procedures/details/[id].tsx
@@ -559,6 +559,7 @@ console.log(e.target.value);
                       init={{
                         height: 500,
                         menubar: true,
+                        selector: 'textarea',
                         plugins: [
                           'advlist', 'autolink', 'lists', 'link', 'image', 'charmap', 'preview',
                           'anchor', 'searchreplace', 'visualblocks', 'code', 'fullscreen',
@@ -572,6 +573,28 @@ console.log(e.target.value);
                     image_title: true,
                     automatic_uploads: true,
                     file_picker_types: "image",
+                    file_picker_callback: function (cb, value, meta) {
+                      var input = document.createElement("input");
+                      input.setAttribute("type", "file");
+                      input.setAttribute("accept", "image/*");
+                      input.onchange = function () {
+                        var file = this.files[0];
+          
+                        var reader = new FileReader();
+                        reader.onload = function () {
+                          var id = "blobid" + new Date().getTime();
+                          var blobCache =
+                            window?.tinymce?.activeEditor.editorUpload.blobCache;
+                          var base64 = reader?.result?.split(",")[1];
+                          var blobInfo = blobCache.create(id, file, base64);
+                          blobCache.add(blobInfo);
+                          cb(blobInfo.blobUri(), { title: file.name });
+                        };
+                        reader.readAsDataURL(file);
+                      };
+          
+                      input.click();
+                    },
                     setup: function (editor) {
 
                       editor.ui.registry.addButton("customInsertButton", {
