88e8672623c6a184a2caa7442356e6420f41705b
kalaivani <kalaivani.ganesan@twilightsoftwares.com>
2024-01-31 17:24:37 +0530

    issue fixed in profile page

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -321,7 +321,7 @@ const Login = () => {
           className="forgot-pass1"
 
         >
-          version 2. 4. 3
+          version 2. 4. 4
         </Typography>
       </Box>
       <Box sx={{ mt: "2rem" }}>
