4224e17f171306d064a9ec1a3d3e31585599364e
kalaivani <kalaivani.ganesan@twilightsoftwares.com>
2024-02-06 16:05:19 +0530

    client side bugs fixed

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -321,7 +321,7 @@ const Login = () => {
           className="forgot-pass1"
 
         >
-          version 2. 4. 7
+          version 2. 4. 8
         </Typography>
       </Box>
       <Box sx={{ mt: "2rem" }}>
